98013b0d0040c0ca442f2a2bfb826bff884025b8
olni0002 <olni0002@stud.kea.dk>
2024-12-16 05:02:40 +0100

    Add custom query for deleting category images by id

--- src/main/java/dk/nailsalondeluxe/backend/repository/CategoryImageRepository.java
+++ src/main/java/dk/nailsalondeluxe/backend/repository/CategoryImageRepository.java
@@ -3,10 +3,16 @@ package dk.nailsalondeluxe.backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import dk.nailsalondeluxe.backend.model.CategoryImage;
 
 public interface CategoryImageRepository extends JpaRepository<CategoryImage, Integer> {
 
     Optional<CategoryImage> findByCategoryId(int categoryId);
+
+    @Modifying
+    @Query(value = "DELETE FROM category_image WHERE id=:id", nativeQuery = true)
+    void deleteById(int id);
 }
